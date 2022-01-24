cd3d37274abc2ac63eec6e4fb419e7d5e712a107
Huypro <huyprowow@gmail.com>
2022-01-24 14:18:02 +0700

    excercise

---
+++ server.js
@@ -0,0 +1,58 @@
+//my page is number 7426 :))) in this example
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dns = require("dns");
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+/**
+ * process.cwd() trả về thư mục làm việc hiện tại, (thư mục mà đã gọi lệnh node từ đó).
+ * __dirname trả về tên thư mục của thư mục chứa tệp mã nguồn JavaScript
+ */
+app.use("/public", express.static(process.cwd() + "/public"));
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
+
+const listeners = app.listen(port, () => console.log(`Listen port ${port}`));
+
+// dns.lookup(host, cb) //verify submit url
+/**
+ * Về mặt đồng thời(concurrency) , tốt hơn nên sử dụng dns.resolve*() vì các yêu cầu đó không kết thúc trong nhóm luồng(threadpool),
+ * trong khi các yêu cầu dns.lookup() thực hiện vì chúng gọi ra trình phân giải DNS của hệ điều hành thường chặn (mặc dù hiện nay có một số loại giao diện không đồng bộ - - nhưng chúng không nhất thiết phải được thực hiện ở mọi nơi).
+ * Hiện tại, node sử dụng nội bộ dns.lookup() cho bất kỳ phân giải DNS tự động nào, chẳng hạn như khi bạn chuyển tên máy chủ cho http.request().
+ */
+let arr = [];
+app.post("/api/shorturl", (req, res) => {
+  // console.log(typeof req.body.url);
+
+  dns.resolve(req.body.url, (err, address) => {
+    if (err) {
+      res.json({ error: "invalid url" });
+    }
+    arr.push(req.body.url);
+    res.json({ original_url: req.body.url, short_url: arr.length });
+  });
+  // console.log(arr);
+});
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const short_url = req.params.short_url;
+  // console.log(typeof short_url);
+  const regex = /^\d+$/;
+  if (regex.test(short_url) || short_url === "0") {
+    const num_short_url = +short_url;
+    if (num_short_url > arr.length) {
+      res.json({ error: "no short url found" });
+    } else {
+      res.redirect(arr[num_short_url - 1]);
+    }
+  } else {
+    res.json({ error: "wrong format" });
+  }
+});
